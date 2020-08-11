177cd3638817959613afbb383aa71448d6c55af9
Kajal764 <kajaldw666@gmail.com>
2020-08-11 19:00:22 +0530

    Added: Form Using useFormik

---
+++ src/Theme.js
@@ -0,0 +1,26 @@
+import {green, red} from '@material-ui/core/colors';
+import {createMuiTheme} from '@material-ui/core/styles';
+
+// A custom theme for this app
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: '#556cd6',
+            contrastText: '#fff'
+        },
+        secondary: {
+            main: '#19857b',
+        },
+        success: {
+            main: green['800']
+        },
+        error: {
+            main: red.A400,
+        },
+        background: {
+            default: 'red',
+        },
+    },
+});
+
+export default theme;
