c6a576fd7a77242b8ce3c5ac9f7c449b5d741000
Kajal764 <kajaldw666@gmail.com>
2020-08-12 22:42:20 +0530

    Added: Disable submit button till validation pass

--- src/component/YoutubeForm.js
+++ src/component/YoutubeForm.js
@@ -34,98 +34,104 @@ function YoutubeForm() {
         <Formik
             initialValues={initialValues}
             validationSchema={validationSchema}
-            onSubmit={onSubmit}>
+            onSubmit={onSubmit}
+            validateOnMount>
 
-            <Form>
-
-                <div className='form-control'>
-                    <label htmlFor='name'>Name</label>
-                    <Field type='text' id='name' name='name' />
-                    <ErrorMessage name='name' component={TextError} />
-                </div>
-
-                <div className='form-control'>
-                    <label htmlFor='email'>E-mail</label>
-                    <Field type='text' id='email' name='email' />
-                    <ErrorMessage name='email'>
-                        {errormsg => <div className='error'>{errormsg}</div>}
-                    </ErrorMessage>
-                </div>
-
-                <div className='form-control'>
-                    <label htmlFor='channel'>Channel</label>
-                    <Field type='text' id='channel' name='channel' placeholder='youtube channel' />
-                    <ErrorMessage name='channel' component={TextError} />
-                </div>
-
-                <div className='form-control'>
-                    <label htmlFor='comments'>Comments</label>
-                    <Field as='textarea' id='comments' name='comments' />
-                </div>
-
-                <div className='form-control'>
-                    <label htmlFor='address'>Address</label>
-                    <FastField name='address'>
-                        {props => {
-                            const { field, meta } = props                            
-                            return (
-                                <div>
-                                    <input type='text' id='address' {...field} />
-                                    {meta.touched && meta.error ? <div>{meta.error}</div> : null}
-                                </div>
-                            )
-                        }
-                        }
-                    </FastField>
-                </div>
-
-                <div className='form-control'>
-                    <label htmlFor='facebook'>Facebook</label>
-                    <Field type='text' id='facebook' name='social.facebook' /> </div>
-
-                <div className='form-control'>
-                    <label htmlFor='twitter'>Twitter</label>
-                    <Field type='text' id='twitter' name='social.twitter' /> </div>
-
-                <div className='form-control'>
-                    <label >List of phone numbers</label>
-                    <FieldArray name='phoneNumber' >
-                        {
-                            fieldArrayProps => {
-                                const { push, remove, form } = fieldArrayProps;
-                                const { values } = form
-                                const { phoneNumber } = values
-                                 
+           {
+               formik =>{
+                   return(
+                    <Form>
+                    <div className='form-control'>
+                        <label htmlFor='name'>Name</label>
+                        <Field type='text' id='name' name='name' />
+                        <ErrorMessage name='name' component={TextError} />
+                    </div>
+    
+                    <div className='form-control'>
+                        <label htmlFor='email'>E-mail</label>
+                        <Field type='text' id='email' name='email' />
+                        <ErrorMessage name='email'>
+                            {errormsg => <div className='error'>{errormsg}</div>}
+                        </ErrorMessage>
+                    </div>
+    
+                    <div className='form-control'>
+                        <label htmlFor='channel'>Channel</label>
+                        <Field type='text' id='channel' name='channel' placeholder='youtube channel' />
+                        <ErrorMessage name='channel' component={TextError} />
+                    </div>
+    
+                    <div className='form-control'>
+                        <label htmlFor='comments'>Comments</label>
+                        <Field as='textarea' id='comments' name='comments' />
+                    </div>
+    
+                    <div className='form-control'>
+                        <label htmlFor='address'>Address</label>
+                        <FastField name='address'>
+                            {props => {
+                                const { field, meta } = props                            
                                 return (
                                     <div>
-                                        {
-                                            phoneNumber.map((phoneNumber, index) => (
-                                                <div key={index}>
-                                                    <Field name={`phoneNumber[${index}]`} ></Field>
-                                                    {index > 0 && (
-                                                        <button type='button' onClick={() => remove(index)} >
-                                                            {' '} - {' '}
-                                                        </button>
-                                                    )}
-
-                                                    <button type='button' onClick={() => push('')}>
-                                                        {' '} + {' '}
-                                                    </button>
-
-                                                </div>
-                                            ))
-                                        }
-
+                                        <input type='text' id='address' {...field} />
+                                        {meta.touched && meta.error ? <div>{meta.error}</div> : null}
                                     </div>
                                 )
                             }
-                        }
-                    </FieldArray>
-                </div>
-
-                <button type="submit">Submit</button>
-
-            </Form>
+                            }
+                        </FastField>
+                    </div>
+    
+                    <div className='form-control'>
+                        <label htmlFor='facebook'>Facebook</label>
+                        <Field type='text' id='facebook' name='social.facebook' /> </div>
+    
+                    <div className='form-control'>
+                        <label htmlFor='twitter'>Twitter</label>
+                        <Field type='text' id='twitter' name='social.twitter' /> </div>
+    
+                    <div className='form-control'>
+                        <label >List of phone numbers</label>
+                        <FieldArray name='phoneNumber' >
+                            {
+                                fieldArrayProps => {
+                                    const { push, remove, form } = fieldArrayProps;
+                                    const { values } = form
+                                    const { phoneNumber } = values
+                                     
+                                    return (
+                                        <div>
+                                            {
+                                                phoneNumber.map((phoneNumber, index) => (
+                                                    <div key={index}>
+                                                        <Field name={`phoneNumber[${index}]`} ></Field>
+                                                        {index > 0 && (
+                                                            <button type='button' onClick={() => remove(index)} >
+                                                                {' '} - {' '}
+                                                            </button>
+                                                        )}
+    
+                                                        <button type='button' onClick={() => push('')}>
+                                                            {' '} + {' '}
+                                                        </button>
+    
+                                                    </div>
+                                                ))
+                                            }
+    
+                                        </div>
+                                    )
+                                }
+                            }
+                        </FieldArray>
+                    </div>
+    
+                    <button type="submit"  disabled={!formik.isValid || formik.isSubmitting }>Submit</button>
+    
+                </Form>
+                   )
+               }
+           }
         </Formik>
     )
 }
